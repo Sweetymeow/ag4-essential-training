209fdaa03d75f575e2ee3d361f1c8ba2280d0ed2
Wendy <honeykatie@qq.com>
2017-08-17 22:57:57 -0700

    Still have BUG in valid of form

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
@@ -16,7 +16,10 @@ export class MwMediaItemFormComponent {
     this.mdForm = new FormGroup({
       medium: new FormControl('Movies'),
       category: new FormControl(''),
-      name: new FormControl(''),
+      name: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ]),
       year: new FormControl('')
     });
   }
