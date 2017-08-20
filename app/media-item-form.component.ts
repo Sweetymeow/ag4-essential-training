0221b3bb9db233c830380deb7bcef2a8c7efcf67
Wendy <honeykatie@qq.com>
2017-08-19 22:26:15 -0700

    Correct the variable name in HTML to 'myForm' in submit button // and add custom validator for input - Chapter 4-5

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -16,12 +16,25 @@ export class MwMediaItemFormComponent {
     this.mdForm = new FormGroup({
       medium: new FormControl('Movies'),
       category: new FormControl(''),
-      name: new FormControl('', [
-        Validators.required,
-        Validators.pattern('[\\w\\-\\s\\/]+')
-      ]),
-      year: new FormControl('')
+      name: new FormControl('', [ Validators.required, Validators.pattern('[\\w\\-\\s\\/]+')]),
+      year: new FormControl('', [ Validators.required, this.yearValidator])
     });
+  }
+
+  yearValidator(control){
+    if(control.value.trim().length === 0){
+      // check if the value's empty return null - Valid
+      return null;
+    }
+    let year = parseInt(control.value);
+    let minYear = 1900;
+    let maxYear = 2017;
+
+    if(year >= minYear && year <= maxYear){
+      return null;
+    }else{ // message for error
+      return { 'year': true };
+    }
   }
 
   onSubmit(mediaForm){
